a40dc0ffb22c1b890a022c58f8bea990a0509628
Nabil Ghedjati <nabil.ghedjati@gmail.com>
2016-10-13 20:18:17 +0200

    first

---
+++ script.js
@@ -0,0 +1,36 @@
+// Utilisation de jQuery
+
+$(document).ready(function(){
+  var valueField = $('input').val();
+  var cell = $('td:nth-child(2)').val();
+
+  if (cell == valueField)
+    $('tr').hide();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
